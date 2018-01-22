787daaa4f608f323b234ed35fcfd1b44a012a557
Carlos Pereira <carloshpereira27@gmail.com>
2018-01-22 07:55:29 +0000

    Resultados k-means-mpi na google cloud.

--- exec_script.sh
+++ exec_script.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
+# --------------------------------------------------------
+# ---------------- BEGIN Parâmetros ----------------------
+# --------------------------------------------------------
+
 # Lista de base de dados originais
 path_databases=("databases/image/segmentation.data" "databases/image/segmentation.data10Times" "databases/letter-recognition/letter-recognition.data" "databases/letter-recognition/letter-recognition.data10Times")
 
-# Lista de colunas onde as classes estão localizadas
+# Lista de colunas onde as classes estão localizadas (índices)
 class_columns=("0" "0" "0" "0")
 
 # Lista da quantidade de classes nas bases de dados
@@ -10,29 +16,32 @@ class_count=("7" "7" "26" "26")
 # Lista da quantidade de features nas bases de dados
 num_features=("19" "19" "16" "16")
 
-# Quantidade de base de dados testadas
-qty_databases=4
-
 # Quantidade de vezes que executará cada cenário de teste
 qty_test=10
 
-# Quantidade de cenários de números de processos
-# qty_proc_test=12
-qty_proc_test=2
-
 # Lista de quantidade de processos utilizados para cada cenário de teste
-# num_procs=("2" "3" "4" "6" "8" "10" "12" "16" "20" "24" "32" "40")
-num_procs=("2" "4")
+num_procs=("2" "3" "4" "6" "8" "10" "12" "16" "20" "24" "32" "40")
+# num_procs=("2" "4")
 
 # Lista de arquivos de saída csv para os tempos de processamento
 out_files=("reports/segmentation.csv" "reports/segmentation_10times.csv" "reports/letter-recognition.csv" "reports/letter-recognition_10times.csv")
 
+# --------------------------------------------------------
+# ---------------- END Parâmetros ------------------------
+# --------------------------------------------------------
+
+
+
 echo "Compilando projeto k-means-mpi"
 make
 echo "----------------------------------------------------"
+echo
+
+echo "Criando diretório reports ..."
+mkdir reports
 
 echo "Gerando versão binária das base de dados com a aplicação bin_database_writer"
-for i in $(seq 0 $((qty_databases-1))); do
+for i in $(seq 0 $((${#path_databases[@]}-1))); do
     echo "bin/./bin_database_writer ${path_databases[$i]} ${class_columns[$i]}Z"
     bin/./bin_database_writer ${path_databases[$i]} ${class_columns[$i]}
     echo "----------------------------------------------------"
@@ -40,7 +49,7 @@ done
 echo "----------------------------------------------------"
 
 
-for i in $(seq 0 $((qty_databases-1))); do
+for i in $(seq 0 $((${#path_databases[@]}-1))); do
     echo "Criando header do arquivo de saída csv - ${out_files[$i]}"
     echo "time_in,time_report,time_kmeans,time_total,nproc,teste_index" > ${out_files[$i]}
     echo "Executando os testes sequenciais da base ${path_databases[$i]} ..."
@@ -51,13 +60,14 @@ for i in $(seq 0 $((qty_databases-1))); do
     done
     echo "----------------------------------------------------"
     echo "Executando os testes paralelos da base ${path_databases[$i]} ..."
-    for j in $(seq 0 $((qty_proc_test-1))); do
+    for j in $(seq 0 $((${#num_procs[@]}-1))); do
         echo "Executando testes da base ${path_databases[$i]} para ${num_procs[$j]} processos ..."
         for k in $(seq 0 $((qty_test-1))); do
-            echo "mpirun -np ${num_procs[$j]} bin/./kmeans_mpi_app ${path_databases[$i]}.bin ${num_features[$i]} ${class_count[$i]} >> ${out_files[$i]}"
-            mpirun -np ${num_procs[$j]} bin/./kmeans_mpi_app ${path_databases[$i]}.bin ${num_features[$i]} ${class_count[$i]} >> ${out_files[$i]}
+            echo "mpirun -mca btl ^openib -np ${num_procs[$j]} bin/./kmeans_mpi_app ${path_databases[$i]}.bin ${num_features[$i]} ${class_count[$i]} >> ${out_files[$i]}"
+            mpirun -mca btl ^openib -np ${num_procs[$j]} bin/./kmeans_mpi_app ${path_databases[$i]}.bin ${num_features[$i]} ${class_count[$i]} >> ${out_files[$i]}
             echo ",${num_procs[$j]},$k" >> ${out_files[$i]}
         done
     done
     echo "----------------------------------------------------"
+    echo ""
 done
--- install_mpi_and_test.sh
+++ install_mpi_and_test.sh
@@ -1,15 +1,38 @@
+#!/bin/bash
+
+# --------------------------------------------------------
+# ---------------- BEGIN Parâmetros ----------------------
+# --------------------------------------------------------
+
+# Base de dados de teste
+path_database="databases/image/segmentation.data"
+
+# Índice da coluna que contém a classe na base de dados
+class_column="0"
+
+# Quantidade de classes diferentes da base de dados
+class_count="7"
+
+# Quantidade de features das instâncias da base de dados
+num_features="19"
+
+# --------------------------------------------------------
+# ---------------- END Parâmetros ------------------------
+# --------------------------------------------------------
+
+echo "Instalando mpi"
+
 sudo apt-get update
-
 sudo apt-get install mpi-default-dev mpich libopenmpi-dev openmpi-bin
-
-path_databases=("databases/image/segmentation.data")
-class_columns=("0")
-class_count=("7")
-num_features=("19")
 
 echo "Compilando projeto k-means-mpi"
 make
 echo "----------------------------------------------------"
 
-bin/./bin_database_writer ${path_databases[$0]} ${class_columns[$0]}
-mpirun -np 4 bin/./kmeans_mpi_app ${path_databases[$0]}.bin ${num_features[$0]} ${class_count[$0]}
+echo "Testando ..."
+echo "bin/./bin_database_writer $path_database $class_column"
+bin/./bin_database_writer $path_database $class_column
+echo "mpirun -mca btl ^openib -np 4 bin/./kmeans_mpi_app $path_database.bin $num_features $class_count"
+mpirun -mca btl ^openib -np 4 bin/./kmeans_mpi_app $path_database.bin $num_features $class_count
+echo ""
+
